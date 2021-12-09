8a02072c9335de850a5ae95e39edce463bd32ea1
CanoMaria <mcano596@alumnos.iua.edu.ar>
2021-12-09 18:57:49 -0300

    InitApp

---
+++ app/src/main/java/com/iua/tecnomovilesfinal/InitApp.kt
@@ -0,0 +1,11 @@
+package com.iua.tecnomovilesfinal
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InitApp: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activ_login)
+    }
+}
